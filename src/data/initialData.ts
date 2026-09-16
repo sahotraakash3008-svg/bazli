2db7b8de0727dc31d85785f98ee52bc37d8eeb9b
sahotraakash3008-svg <sahotraakash3008@gmail.com>
2026-09-16 23:01:01 +0530

    Create initialData.ts

---
+++ src/data/initialData.ts
@@ -0,0 +1,11 @@
+export const INITIAL_PRODUCTS = [];
+export const INITIAL_STORES = [];
+export const INITIAL_CATEGORIES = [];
+export const INITIAL_ORDERS = [];
+
+export default {
+  products: INITIAL_PRODUCTS,
+  stores: INITIAL_STORES,
+  categories: INITIAL_CATEGORIES,
+  orders: INITIAL_ORDERS,
+};
